b761ac8721ff424de34b0ee0b34483c77e80d0f1
elphie1185 <delphine_rabiller@yahoo.fr>
2019-09-08 18:07:03 +0100

    adding info boxes

--- ui.R
+++ ui.R
@@ -8,48 +8,47 @@ shinyUI(fluidPage(
   
 # Adds in the random bit of javascript that somehow makes the map tab
 # size dynamically change with the sie of the window.
-tags$script(jscode),
+  tags$script(jscode),
   
 # Add theme
-theme = shinythemes::shinytheme("cerulean"),
+  theme = shinythemes::shinytheme("cerulean"),
   
 # Title
-titlePanel(tags$h2("Website Performance")),
+  titlePanel(tags$h2("Website Performance")),
   
 # Creates heading row with date range picker and total sessions and
 # conversions.
-fluidRow(
-  column(4,
-         dateRangeInput("date_range",
-                        label = "Pick a date range",
-                        start = Sys.Date() - 60,
-                        end = Sys.Date(), 
-                        min = as_date("2019-01-01"), 
-                        max = as_date("2019-09-08")
-           )
-    ),
-  column(4,
-         span(tags$h5(textOutput("website_visitors")),
-              align = "center"
+  fluidRow(
+    column(4,
+           dateRangeInput("date_range",
+                          label = "Pick a date range",
+                          start = Sys.Date() - 60,
+                          end = Sys.Date(), 
+                          min = as_date("2019-01-01"), 
+                          max = as_date("2019-09-08")
+            )
+      ),
+    column(4,
+          span(tags$h5(textOutput("website_visitors")),
+                                  align = "center"
          ),
          span(tags$b(textOutput("website_visitors_number")),
               style = "color:orange", align = "center"
          )
-  ),
-  column(4,
-         span(tags$h5(textOutput("conversions")),
+    ),
+    column(4,
+          span(tags$h5(textOutput("conversions")),
               align = "center"
-         ),
-         span(tags$b(textOutput("conversions_number")),
+          ),
+           span(tags$b(textOutput("conversions_number")),
               style = "color:orange", align = "center"
-         )
-  )
-),
+           )
+    )
+  ),
   
 # Add the left hand navigation panel
-navlistPanel(
-  "Choose a visualisation:",
-  widths = c(3, 9),
+  navlistPanel("Choose a visualisation:",
+               widths = c(3, 9),
     
 # ==============================================================================
 #
@@ -57,27 +56,28 @@ navlistPanel(
 #
 # ==============================================================================
 
-tabPanel("Event Goal Overview",
-         fluidRow(
-           column(8,
-                  radioButtons("time_range",
-                               "Display Type",
-                               choices = c("Monthly", "Weekly", "Daily"), 
-                               inline = TRUE
-                  ), 
-                  plotOutput("goal_plot")
-           ),
-           column(4, leafletOutput("map_goal"))
-         ), 
-         fluidRow(
-           column(6, 
-                  plotOutput("device_plot")
-           ), 
-           column(6,
-                  plotOutput("all_sessions")
-           )
+  tabPanel("Event Goal Overview",
+           fluidRow(
+             column(8,
+                    radioButtons("time_range",
+                                 "Display Type",
+                                 choices = c("Monthly", "Weekly", "Daily"), 
+                                 inline = TRUE
+                   ), 
+                    plotOutput("goal_plot")
+            ),
+            column(4, leafletOutput("map_goal"))
+          ), 
+         
+          fluidRow(
+            column(6, 
+                   plotOutput("device_plot")
+            ), 
+            column(6,
+                   plotOutput("all_sessions")
+            )
          )
-),
+  ),
 
 # ==============================================================================
 #
@@ -85,24 +85,27 @@ tabPanel("Event Goal Overview",
 #
 # ==============================================================================
 
-tabPanel("Session Sources\n and Conversion by City",
-         fluidRow(
-           column(6, 
-                  plotOutput("session_channel")
-           ),
-           column(6,
-                  plotOutput("session_social")
-           )
+  tabPanel("Session Sources\n and Conversion by City",
+          fluidRow(
+             column(6, 
+                    plotOutput("session_channel")
+             ),
+           
+              column(6,
+                    plotOutput("session_social")
+              )
          ),
+         
          fluidRow(
-           column(6, 
-                  plotOutput("channel_conversion_comparison")
-           ),
-           column(6,
-                  plotOutput("social_conversion_comparison")
-           )
+              column(6, 
+                     plotOutput("channel_conversion_comparison")
+              ),
+           
+              column(6,
+                    plotOutput("social_conversion_comparison")
+              )
          )
-),
+  ),
 
 # ==============================================================================
 #
@@ -110,8 +113,9 @@ tabPanel("Session Sources\n and Conversion by City",
 #
 # ==============================================================================
 
-tabPanel("Map of Sessions",
-         uiOutput("map_1")),
+  tabPanel("Map of Sessions",
+          uiOutput("map_1")
+  ),
 
 # ==============================================================================
 #
@@ -119,10 +123,10 @@ tabPanel("Map of Sessions",
 #
 # ==============================================================================
     
-tabPanel("Top 20 Sessions and Exits",
-         plotOutput("plot_5a"),
-         plotOutput("plot_5b")
-),
+  tabPanel("Top 20 Sessions and Exits",
+            plotOutput("plot_5a"),
+            plotOutput("plot_5b")
+  ),
     
 # ==============================================================================
 #
@@ -130,6 +134,23 @@ tabPanel("Top 20 Sessions and Exits",
 #
 # ==============================================================================
 
-tabPanel(tags$a("Website Link", href = "https://github.com/elphie1185"))
+  tabPanel(
+    infoBox(title = "Website", 
+            icon = icon("globe"),
+            color = "blue", 
+            href = "https://github.com/elphie1185", 
+            fill = TRUE
+      )
+    ),
+  
+  tabPanel(
+    valueBox(value = "Data",
+            subtitle = "Synthetic Data", 
+            icon = icon("database"),
+            color = "blue",
+            width = 12
+        )
+  )
 )
-))
+)
+)

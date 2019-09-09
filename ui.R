8dac75cbb2e1f5047b262310ba4a4b22beb31eed
elphie1185 <delphine_rabiller@yahoo.fr>
2019-09-09 09:31:52 +0100

    change of side pannel width

--- ui.R
+++ ui.R
@@ -18,66 +18,75 @@ shinyUI(fluidPage(
   
 # Creates heading row with date range picker and total sessions and
 # conversions.
-  fluidRow(
-    column(4,
-           dateRangeInput("date_range",
-                          label = "Pick a date range",
-                          start = Sys.Date() - 60,
-                          end = Sys.Date(), 
-                          min = as_date("2019-01-01"), 
-                          max = as_date("2019-09-08")
+  sidebarLayout(
+    sidebarPanel(
+      width = 3,
+      fluidPage(
+        verticalLayout(
+            dateRangeInput("date_range",
+                         label = "Pick a date range",
+                         start = Sys.Date() - 60,
+                         end = Sys.Date(), 
+                         min = as_date("2019-01-01"), 
+                         max = as_date("2019-09-08")
+            ), 
+            infoBox(title = "Visitors", 
+                    icon = icon("user"),
+                    color = "blue", 
+                    value = textOutput("website_visitors_number"), 
+                    fill = TRUE
+            ),
+            infoBox(title = "Conversion", 
+                    icon = icon("comment-dollar"),
+                    color = "blue", 
+                    value = textOutput("conversions_number"), 
+                    fill = TRUE
+            ),
+            infoBox(title = "Website", 
+                    icon = icon("globe"),
+                    color = "blue", 
+                    href = "https://github.com/elphie1185", 
+                    fill = TRUE
+             ),
+            valueBox(value = "Data",
+                     subtitle = "Synthetic Data", 
+                    icon = icon("database"),
+                    color = "blue"
             )
-      ),
-    column(4,
-          span(tags$h5(textOutput("website_visitors")),
-                                  align = "center"
-         ),
-         span(tags$b(textOutput("website_visitors_number")),
-              style = "color:orange", align = "center"
-         )
-    ),
-    column(4,
-          span(tags$h5(textOutput("conversions")),
-              align = "center"
-          ),
-           span(tags$b(textOutput("conversions_number")),
-              style = "color:orange", align = "center"
-           )
-    )
+        )
+      )
   ),
   
-# Add the left hand navigation panel
-  navlistPanel("Choose a visualisation:",
-               widths = c(3, 9),
     
 # ==============================================================================
 #
 # Tab 1 - Event Goal Overview
 #
 # ==============================================================================
-
-  tabPanel("Event Goal Overview",
-           fluidRow(
-             column(8,
-                    radioButtons("time_range",
-                                 "Display Type",
-                                 choices = c("Monthly", "Weekly", "Daily"), 
-                                 inline = TRUE
-                   ), 
-                    plotOutput("goal_plot")
-            ),
-            column(4, leafletOutput("map_goal"))
-          ), 
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Event Goal Overview",
+              fluidRow(
+                  column(8,
+                        radioButtons("time_range",
+                                    "Display Type",
+                                    choices = c("Monthly", "Weekly", "Daily"), 
+                                    inline = TRUE
+                        ), 
+                        plotOutput("goal_plot")
+                  ),
+                  column(4, leafletOutput("map_goal"))
+              ), 
          
-          fluidRow(
-            column(6, 
-                   plotOutput("device_plot")
-            ), 
-            column(6,
-                   plotOutput("all_sessions")
-            )
-         )
-  ),
+              fluidRow(
+                      column(6, 
+                            plotOutput("device_plot")
+                      ), 
+                      column(6,
+                            plotOutput("all_sessions")
+                      )
+              )
+            ),
 
 # ==============================================================================
 #
@@ -85,7 +94,7 @@ shinyUI(fluidPage(
 #
 # ==============================================================================
 
-  tabPanel("Session Sources\n and Conversion by City",
+        tabPanel("Session Sources\n and Conversion by City",
           fluidRow(
              column(6, 
                     plotOutput("session_channel")
@@ -94,7 +103,7 @@ shinyUI(fluidPage(
               column(6,
                     plotOutput("session_social")
               )
-         ),
+          ),
          
          fluidRow(
               column(6, 
@@ -105,7 +114,7 @@ shinyUI(fluidPage(
                     plotOutput("social_conversion_comparison")
               )
          )
-  ),
+      ),
 
 # ==============================================================================
 #
@@ -113,9 +122,9 @@ shinyUI(fluidPage(
 #
 # ==============================================================================
 
-  tabPanel("Map of Sessions",
-          uiOutput("map_1")
-  ),
+      tabPanel("Map of Sessions",
+              uiOutput("map_1")
+      ),
 
 # ==============================================================================
 #
@@ -123,34 +132,12 @@ shinyUI(fluidPage(
 #
 # ==============================================================================
     
-  tabPanel("Top 20 Sessions and Exits",
-            plotOutput("plot_5a"),
-            plotOutput("plot_5b")
-  ),
-    
-# ==============================================================================
-#
-# Tab 5 - Website link
-#
-# ==============================================================================
-
-  tabPanel(
-    infoBox(title = "Website", 
-            icon = icon("globe"),
-            color = "blue", 
-            href = "https://github.com/elphie1185", 
-            fill = TRUE
+      tabPanel("Top 20 Sessions and Exits",
+                plotOutput("plot_5a"),
+                plotOutput("plot_5b")
       )
-    ),
-  
-  tabPanel(
-    valueBox(value = "Data",
-            subtitle = "Synthetic Data", 
-            icon = icon("database"),
-            color = "blue",
-            width = 12
-        )
+    )
   )
-)
+  )
 )
 )
